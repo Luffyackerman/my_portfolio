310f06aa9a06538999786fdc86b3ac9ccd0098f5
John Michael D. Echemani <112184334+Luffyackerman@users.noreply.github.com>
2023-05-30 22:02:09 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  purge: [],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
+
